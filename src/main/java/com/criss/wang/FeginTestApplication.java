c1b2f74008e59a98e32b15b92ca2b3d7a17dbd55
wangqiubao <wangqiubao@u-care.net>
2020-02-14 16:50:03 +0800

    加了点代码

--- src/main/java/com/criss/wang/FeginTestApplication.java
+++ src/main/java/com/criss/wang/FeginTestApplication.java
@@ -2,13 +2,14 @@ package com.criss.wang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 import com.criss.wang.main.FeginApiApplication;
 
 @Import({FeginApiApplication.class})
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @ComponentScan({
 	"com.criss.wang.controller.**",
 	"com.criss.wang.service.**"
